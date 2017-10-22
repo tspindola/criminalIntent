d55cb1a3792864d0ecbd8022ae93458d96dcd725
Thiago Spíndola <peza182@gmail.com>
2017-10-22 01:07:29 -0200

    Created recycler view adapter and holder, and updated the crimeFragment to show the list of crimes

--- app/src/main/java/com/example/thiag/criminalintent/CrimeListFragment.java
+++ app/src/main/java/com/example/thiag/criminalintent/CrimeListFragment.java
@@ -8,18 +8,58 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.List;
+
 /**
  * Created by thiag on 19/10/2017.
  */
 
 public class CrimeListFragment extends Fragment {
     private RecyclerView mCrimeRecyclerView;
+    private CrimeAdapter mCrimeAdapter;
+
+    private class CrimeHolder extends RecyclerView.ViewHolder{
+        public CrimeHolder(LayoutInflater inflater,ViewGroup parent){
+            super(inflater.inflate(R.layout.list_item_crime,parent,false));
+        }
+    }
+
+    private class CrimeAdapter extends RecyclerView.Adapter<CrimeHolder>{
+        private List<Crime> mCrimes;
+        public CrimeAdapter(List<Crime> crimes){
+            mCrimes = crimes;
+        }
+
+        @Override
+        public CrimeHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            LayoutInflater layoutInflater = LayoutInflater.from(getActivity());
+            return new CrimeHolder(layoutInflater,parent);
+        }
+
+        @Override
+        public void onBindViewHolder(CrimeHolder holder, int position) {
+
+        }
+
+        @Override
+        public int getItemCount() {
+            return mCrimes.size();
+        }
+    }
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState){
         View view = inflater.inflate(R.layout.fragment_crime_list,container,false);
         mCrimeRecyclerView = (RecyclerView) view.findViewById(R.id.crime_recycler_view);
         mCrimeRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        UpdateUI();
         return view;
+    }
+
+    private void UpdateUI(){
+        CrimeLab crimeLab = CrimeLab.get(getActivity());
+        List<Crime> crimes = crimeLab.getCrimes();
+        mCrimeAdapter = new CrimeAdapter(crimes);
+        mCrimeRecyclerView.setAdapter(mCrimeAdapter);
     }
 }
